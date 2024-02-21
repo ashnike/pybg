b015ef391f802e0d65c9fe82cec077b7132f48f7
Ashnil <85815731+ashnike@users.noreply.github.com>
2024-02-21 13:17:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def index():
         return render_template('error.html', message='Template not found!')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=false)
+    app.run(host='0.0.0.0', debug=False)
 
