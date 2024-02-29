b0d55e0edf91af3b8921089ca7942bbc0ecd1e70
Ashnil <85815731+ashnike@users.noreply.github.com>
2024-02-29 13:50:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,14 @@ app.config['TEMPLATES'] = os.path.join(os.path.dirname(__file__), 'templates')
 
 metrics = PrometheusMetrics(app)
 
+
+request_count = metrics.define_counter(
+    'flask_app_requests_total', 'Total number of requests served by the app'
+)
+app_health = metrics.define_gauge(
+    'flask_app_health', 'Health status of the Flask application (1: healthy, 0: unhealthy)', 1
+)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     heading = "My Flask App"  # Initial heading
@@ -26,6 +34,15 @@ def index():
         return render_template('index.html', heading=heading, inputs=inputs)
     except TemplateNotFound:
         return render_template('error.html', message='Template not found!')
+
+@app.route('/health')
+def health_check():
+    if True:  
+        app_health.set(1)  # Set metric to 1 (healthy)
+        return 'App is healthy'
+    else:
+        app_health.set(0)  # Set metric to 0 (unhealthy)
+        return 'App is unhealthy', 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False)
